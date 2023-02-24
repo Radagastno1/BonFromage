266b5cbe6ec27df153b69f929488a936a02df737
Angelina Holmqvist <angelinaholmqvist@live.se>
2023-02-24 22:29:37 +0100

    controller route name

--- Controllers/MenuController.cs
+++ Controllers/MenuController.cs
@@ -4,7 +4,7 @@ using BonFromage.Models;
 using Microsoft.AspNetCore.Html;
 
 namespace BonFromage.Controllers;
-
+  [Route("menu")]
 public class MenuController : Controller
 {
     private readonly ILogger<MenuController> _logger;
@@ -15,7 +15,6 @@ public class MenuController : Controller
     }
 
      [HttpGet]
-     [Route("menu")]
     public ActionResult Index()
     {
         List<Dish>Dishes = new(){new Dish("Coq au Vin ","En gryta med kyckling tillagad med rött vin, champinjoner och bacon.", 179), new Dish("Bouillabaisse ","En fisk- och skaldjursgryta med tomat, fänkål, vitlök och saffran.", 125), new Dish("Ratatouille","En grönsaksgryta med aubergine, zucchini, paprika, tomater och lök.", 119)};
